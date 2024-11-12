cd6a9998786d4e3b89f7c924b6a582e756d04de1
vishale <vishalingle9876@gmail.comm>
2024-11-12 17:56:55 +0530

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,58 @@
+"use client"
 import Image from "next/image";
-import styles from "./page.module.css";
+
+//import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   
+  const myset = [[1,2,3] , [4,5,6]]
+  const [turn , setTurn] = useState("p1");
+  
+  let a= true;
+  const fillbox = (a)=>{
+    if(turn == "p1"){
+      // document.getElementById(a).innerHTML="o";
+      a.innerHTML="O"
+      setTurn("p2")
+      a=false
+    console.log(turn)
+    }
+    else{
+      // document.getElementById(a).innerHTML="x";
+      a.innerHTML="X"
+      setTurn("p1")
+    }
+  }
+  var boxes;
+  useEffect(()=>{
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+   var boxes = document.querySelectorAll(".box")
+  //  // const a = boxes.addEventListener("click",myfun);
+   boxes.forEach((box) => {
+    box.addEventListener("click",()=> {fillbox(box)});
+  });
+  }, [] )
+console.log("a is ",a);
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  return (<>
+  {/* {fillbox()} */}
+   <h1>Tic tac toe</h1>
+  <div id="con">
+ 
+  <div className="box" id="box0" onClick={()=>fillbox("box0")}> </div>
+  <div className="box" id="box1"> </div>
+  <div className="box" > </div>
+  <div className="box" > </div>
+  <div className="box" > </div>
+  <div className="box" > </div>
+  <div className="box" > </div>
+  <div className="box" > </div>
+  <div className="box"> </div>
+  </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  
+  </>
+  
   );
 }
