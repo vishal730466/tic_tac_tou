163dfb98034ee21c9b6eae8de89b2bd365092af2
vishale <vishalingle9876@gmail.comm>
2024-11-13 23:18:34 +0530

    winner

--- src/app/page.js
+++ src/app/page.js
@@ -6,49 +6,66 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
    
-  const myset = [[1,2,3] , [4,5,6]]
-  const [turn , setTurn] = useState("p1");
+  const myset = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ]
+  const [turn , setTurn] = useState("Player1");
+  const [winner, setWinner] = useState(false)
   
-  let a= true;
+  
+  
   const fillbox = (a)=>{
-    if(turn == "p1"){
-      // document.getElementById(a).innerHTML="o";
-      a.innerHTML="O"
-      setTurn("p2")
-      a=false
-    console.log(turn)
+
+    if(document.getElementById(a).innerHTML != "O" && document.getElementById(a).innerHTML!="X"){
+
+      if(turn == "Player1"){
+        document.getElementById(a).innerHTML="O";
+        check_winner();
+        setTurn("Player2")
+      }
+      else{
+        document.getElementById(a).innerHTML="X";
+        var a=check_winner();
+        console.log(a)
+      setTurn("Player1")
+      }
+
     }
-    else{
-      // document.getElementById(a).innerHTML="x";
-      a.innerHTML="X"
-      setTurn("p1")
+    
+  }
+
+  const check_winner=()=>{
+    
+   var boxes = document.querySelectorAll(".box")
+    for (var i in myset) {
+      var subArray = myset[i];
+
+      var position1=boxes[subArray[0]].innerHTML
+      var position2=boxes[subArray[1]].innerHTML
+      var position3=boxes[subArray[2]].innerHTML
+
+      console.log(position1,position2,position3)
+      if(position1===position2 && position2===position3 && position1!=" "){
+        setWinner(true);
+        console.log("winner")
+        return(position1)
+      }
+        // console.log(i[0],i[1],i[2])   not working
     }
   }
-  var boxes;
-  useEffect(()=>{
-
-   var boxes = document.querySelectorAll(".box")
-  //  // const a = boxes.addEventListener("click",myfun);
-   boxes.forEach((box) => {
-    box.addEventListener("click",()=> {fillbox(box)});
-  });
-  }, [] )
-console.log("a is ",a);
 
   return (<>
-  {/* {fillbox()} */}
+  
    <h1>Tic tac toe</h1>
   <div id="con">
- 
+   {winner && <div id="winner"> winner is {turn} </div>}
   <div className="box" id="box0" onClick={()=>fillbox("box0")}> </div>
-  <div className="box" id="box1"> </div>
-  <div className="box" > </div>
-  <div className="box" > </div>
-  <div className="box" > </div>
-  <div className="box" > </div>
-  <div className="box" > </div>
-  <div className="box" > </div>
-  <div className="box"> </div>
+  <div className="box" id="box1" onClick={()=>fillbox("box1")}> </div>
+  <div className="box" id="box2" onClick={()=>fillbox("box2")}> </div>
+  <div className="box" id="box3" onClick={()=>fillbox("box3")}> </div>
+  <div className="box" id="box4" onClick={()=>fillbox("box4")}> </div>
+  <div className="box" id="box5" onClick={()=>fillbox("box5")}> </div>
+  <div className="box" id="box6" onClick={()=>fillbox("box6")}> </div>
+  <div className="box" id="box7" onClick={()=>fillbox("box7")}> </div>
+  <div className="box" id="box8" onClick={()=>fillbox("box8")}> </div>
   </div>
 
   
